9890655e6add5bdaef988b61d82f89567e2ed2d5
KohKoh-Nut <kohkohnut1202@gmail.com>
2026-08-22 14:53:08 +0800

    Complete Level 1 command echo and exit behavior

--- src/main/java/Zabud.java
+++ src/main/java/Zabud.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Zabud {
     public static void main(String[] args) {
         String separator = "____________________________________________________________";
@@ -14,7 +16,19 @@ public class Zabud {
         System.out.println("Hello, King Solomon! I'm Zabud, your royal assistant.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye, King Solomon. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+            System.out.println(" " + command);
+            System.out.println(separator);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye, King Solomon. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+        }
     }
 }
